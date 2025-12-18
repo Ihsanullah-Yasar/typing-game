b43790bfd8d80bc0b779363b8568fe993264db9c
Ihsanullah-Yasar <ihsanullahyasar70@gmail.com>
2025-12-18 21:41:39 +0430

    word logic changed

--- src/words.js
+++ src/words.js
@@ -1,14 +1,56 @@
 const WORDS = [
-  'apple','banana','orange','grape','melon','keyboard','javascript','module','request','animation','frame','performance','function','variable','constant','array','object','style','layout','accessibility','focus','event','listener','promise','async','await','thread','render','viewport','design','pattern','component','state','score','lives','wpm','typing','speed','race','challenge','practice','effort','skill'
+  "apple",
+  "banana",
+  "orange",
+  "grape",
+  "melon",
+  "keyboard",
+  "javascript",
+  "module",
+  "request",
+  "animation",
+  "frame",
+  "performance",
+  "function",
+  "variable",
+  "constant",
+  "array",
+  "object",
+  "style",
+  "layout",
+  "accessibility",
+  "focus",
+  "event",
+  "listener",
+  "promise",
+  "async",
+  "await",
+  "thread",
+  "render",
+  "viewport",
+  "design",
+  "pattern",
+  "component",
+  "state",
+  "score",
+  "lives",
+  "wpm",
+  "typing",
+  "speed",
+  "race",
+  "challenge",
+  "practice",
+  "effort",
+  "skill",
 ];
 
 export default class Words {
-  constructor(list = WORDS){
+  constructor(list = WORDS) {
     this.list = list.slice();
   }
 
-  random(){
-    const i = Math.floor(Math.random()*this.list.length);
+  random() {
+    const i = Math.floor(Math.random());
     return this.list[i];
   }
 }
